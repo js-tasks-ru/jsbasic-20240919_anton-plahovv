03ac7475e2ceaf1ccb4272874abc21cd4f624e88
Anton Plakhov <anton.plahovv@gmail.com>
2024-09-30 17:07:36 +0300

    Задачи 1.4 1.5 2.1

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,12 @@
 function sumSalary(salaries) {
   // ваш код...
+  let rez = 0;
+
+  for (let key in salaries) {
+    if (Number.isFinite(salaries[key])) {
+      rez += salaries[key];
+    }
+  }
+
+  return rez;
 }
