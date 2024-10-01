51a84853c789ce2b3d3de82e805ccdfe3be6e039
Anton Plakhov <anton.plahovv@gmail.com>
2024-10-01 20:34:17 +0300

    Задачи 2.2 2.3

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,17 @@
 let calculator = {
   // ваш код
+  a: 0,
+  b: 0,
+  read: function(num1, num2) {
+    this.a = num1;
+    this.b = num2;
+  },
+  sum: function() {
+    return this.a + this.b;
+  },
+  mul: function() {
+    return this.a * this.b;
+  },
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
