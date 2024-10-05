3c57156c60914216d9ffa3bce290d27f0395ea2e
Anton Plakhov <anton.plahovv@gmail.com>
2024-10-05 17:54:49 +0300

    commit task 3.4

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,10 @@
 function showSalary(users, age) {
   // ваш код...
+  let filtredUsers = users
+    .filter(user => user.age <= age)
+    .map(user => `${user.name}, ${user.balance}`);
+  //let resultUsers = filtredUsers.map(user => `${user.name}, ${user.balance}`);
+
+  return filtredUsers.join('\n');
+
 }
