6d27a6dd47116417128a88e365bf254435ae707e
Anton Plakhov <anton.plahovv@gmail.com>
2024-10-09 20:30:49 +0300

    task 4.1

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,17 @@
 function makeFriendsList(friends) {
   // ваш код...
+  let ulFrendsList = document.createElement('ul');
+
+  //let liFriend = document.createElement('li');
+
+  for (let friend of friends) {
+    // Добавление через innerHTML
+    //ulFrendsList.innerHTML += `<li>${friend.firstName} ${friend.lastName}/li>`;
+    // Через объект
+    let liFriend = document.createElement('li'); // Почему когда я выношу объявление переменной liFriend за цикл у меня добавляется только последний из списка друзей? 
+    liFriend.textContent = `<li>${friend.firstName} ${friend.lastName}/li>`;
+    ulFrendsList.append(liFriend);
+  }
+
+  return ulFrendsList;
 }
