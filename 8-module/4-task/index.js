65deac7f5262f94a6179f13cdaeedc69d4439ec5
Anton Plakhov <anton.plahovv@gmail.com>
2024-10-29 19:41:59 +0300

    task 8.4 возникли проблемы при автоматической проверке в методе onSubmit()

--- 8-module/4-task/index.js
+++ 8-module/4-task/index.js
@@ -14,29 +14,65 @@ export default class Cart {
 
   addProduct(product) {
     // СКОПИРУЙТЕ СЮДЯ СВОЙ КОД
+    let cartItem = null;
+    //1. Проверка на пустоту product
+    if (product === null || product === undefined) { return; }
+
+    //2. Проверка есть ли товар в корзине cartItems
+    let cartIndex = this.cartItems.findIndex(item =>
+      item.product.id === product.id
+    );
+
+    if (cartIndex != -1) {
+      this.cartItems[cartIndex].count++;
+      cartItem = this.cartItems[cartIndex];
+    }
+    else {
+      this.cartItems.push({ 'product': product, 'count': 1 });
+      cartItem = ({ 'product': product, 'count': 1 });
+    }
+    //3. 
+    this.onProductUpdate(cartItem);
   }
 
   updateProductCount(productId, amount) {
     // СКОПИРУЙТЕ СЮДЯ СВОЙ КОД
+    let cartItem = null;
+    //1.Обновить количество единиц товара в массиве cartItems.
+    let cartIndex = this.cartItems.findIndex(item =>
+      item.product.id === productId
+    );
+    this.cartItems[cartIndex].count += amount;
+    cartItem = this.cartItems[cartIndex];
+    //2.Если количество стало 0, то этот товар нужно удалить из корзины.
+    if (this.cartItems[cartIndex].count === 0) {
+      this.cartItems.splice(cartIndex, 1);
+    }
+    //3. 
+    this.onProductUpdate(cartItem);
   }
 
   isEmpty() {
     // СКОПИРУЙТЕ СЮДЯ СВОЙ КОД
+    let result = this.cartItems.length === 0;
+    return result;
   }
 
   getTotalCount() {
     // СКОПИРУЙТЕ СЮДЯ СВОЙ КОД
+    let summ = this.cartItems.reduce((sum, current) => sum + current.count, 0);
+    return summ;
   }
 
   getTotalPrice() {
     // СКОПИРУЙТЕ СЮДЯ СВОЙ КОД
+    let summ = this.cartItems.reduce((sum, current) => sum + current.product.price * current.count, 0);
+    return summ;
   }
 
   renderProduct(product, count) {
     return createElement(`
-    <div class="cart-product" data-product-id="${
-      product.id
-    }">
+    <div class="cart-product" data-product-id="${product.id}">
       <div class="cart-product__img">
         <img src="/assets/images/products/${product.image}" alt="product">
       </div>
@@ -73,9 +109,7 @@ export default class Cart {
         <div class="cart-buttons__buttons btn-group">
           <div class="cart-buttons__info">
             <span class="cart-buttons__info-text">total</span>
-            <span class="cart-buttons__info-price">€${this.getTotalPrice().toFixed(
-              2
-            )}</span>
+            <span class="cart-buttons__info-price">€${this.getTotalPrice().toFixed(2)}</span>
           </div>
           <button type="submit" class="cart-buttons__button btn-group__button button">order</button>
         </div>
@@ -83,19 +117,125 @@ export default class Cart {
     </form>`);
   }
 
+  renderSuccessOrderForm() {
+    return createElement(`
+      <div class="modal__body-inner">
+        <p>
+          Order successful! Your order is being cooked :) <br>
+          We’ll notify you about delivery time shortly.<br>
+          <img src="/assets/images/delivery.gif">
+        </p>
+      </div>
+      `);
+  }
+
   renderModal() {
     // ...ваш код
+    this.modal = new Modal();
+    let bodyModal = createElement('<div></div>');
+
+    this.cartItems.forEach((item) => {
+      bodyModal.appendChild(this.renderProduct(item.product, item.count));
+    });
+
+    bodyModal.appendChild(this.renderOrderForm());
+
+    this.modal.setTitle('Your order');
+    this.modal.setBody(bodyModal);
+    this.modal.open();
+
+    //Добавляем обработчик
+    bodyModal.addEventListener('click', (event) => {
+      let productId = null;
+      if (event.target.closest('.cart-counter__button_plus')) {
+        productId = event.target.closest('.cart-product').dataset.productId;
+        this.updateProductCount(productId, 1);
+      }
+      else if (event.target.closest('.cart-counter__button_minus')) {
+        productId = event.target.closest('.cart-product').dataset.productId;
+        this.updateProductCount(productId, -1);
+      }
+    });
+
+    document.querySelector('.cart-form').addEventListener('submit', (event) => {
+      
+      this.onSubmit = this.onSubmit.bind(this);
+      this.onSubmit(event);
+    });
+    /*
+    bodyModal.addEventListener('submit', (event) => {
+      event.preventDefault();
+      if (event.target.closest('.cart-form')) {
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onSubmit(event);
+
+      }
+    });*/
   }
 
   onProductUpdate(cartItem) {
     // ...ваш код
 
     this.cartIcon.update(this);
+    // Если корзина закрыта - не обрабатываем
+    let body = document.querySelector('body');
+    if (!body.classList.contains('is-modal-open')) { return; }
+
+
+    let cartItems = body.querySelector(`.cart-product[data-product-id="${cartItem.product.id}"]`);
+    let productCount = cartItems.querySelector('.cart-counter__count');
+    let productPrice = cartItems.querySelector('.cart-product__price');
+    let infoPrice = document.querySelector(`.cart-buttons__info-price`);
+    // Закрываем пустую корзину
+    if (this.cartItems.length == 0) {
+      this.modal.close();
+      return;
+    }
+    //1.Удаляем товар при count === 0
+    else if (cartItem.count === 0) {
+      cartItems.remove(cartItems);
+    }
+    else {
+      //2.изменям число товара на актуальное 
+      productCount.textContent = cartItem.count;
+      productPrice.textContent = `€${(cartItem.count * cartItem.product.price).toFixed(2)}`;
+    }
+
+    infoPrice.textContent = `€${this.getTotalPrice().toFixed(2)}`;
   }
 
   onSubmit(event) {
     // ...ваш код
-  };
+    event.preventDefault();
+    let buttonSubmit = event.target.querySelector('.cart-buttons__button');
+    buttonSubmit.classList.add('is-loading');
+
+    let formData = new FormData(document.querySelector('.cart-form'));
+
+    fetch('https://httpbin.org/post', {
+      method: 'POST',
+      body: formData
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      }).then(data => {
+        this.cartItems = [];
+        this.cartIcon.update(this);
+        this.modal.setTitle('Success!');
+        this.modal.setBody(this.renderSuccessOrderForm());
+        console.log('Ответ от сервера:', data);
+      }).catch(error => {
+        console.error('Ошибка:', error.message);
+      });
+    /*// для прохождения тестов 
+    this.cartItems = [];
+    this.cartIcon.update(this);
+    this.modal.setTitle('Success!');
+    this.modal.setBody(this.renderSuccessOrderForm());*/
+  }
 
   addEventListeners() {
     this.cartIcon.elem.onclick = () => this.renderModal();
